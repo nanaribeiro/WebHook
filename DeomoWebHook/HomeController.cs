7a31c3fb24d81990ac06baf1e63bd4c81ac36970
nanaribeiro <aranaribeiro@hotmail.com>
2020-09-08 15:51:52 -0300

    NOVO

---
+++ DeomoWebHook/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DeomoWebHook
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(JObject data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            return Ok();
+        }
+    }
+}
